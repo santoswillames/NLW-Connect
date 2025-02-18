bdd69ca74aa35a98d62eb8e772e26b38a99560a6
Willames Santos <willamessantos@live.com>
2025-02-18 10:55:05 -0300

    aula 01

---
+++ src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,34 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'Subscribe someone to the event ',
+        tags: ['subscription'],
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body
+
+      // criação da inscrição no banco de dados
+
+      return reply.status(201).send({
+        name,
+        email,
+      })
+    }
+  )
+}
